8edf7957e893eb7b4e5c9b9344093d6bda43ae71
kulykivska <kulykivska.yuliya@gmail.com>
2022-02-18 14:39:59 +0200

    refactoring for linage template

--- src/app/dataset-view/dataset.module.ts
+++ src/app/dataset-view/dataset.module.ts
@@ -21,6 +21,7 @@ import { MatTabsModule } from "@angular/material/tabs";
 import {MonacoEditorModule} from "ngx-monaco-editor";
 import {DataComponent} from "./additional-components/data-component/data-component";
 import {OverviewComponent} from "./additional-components/overview-component/overview-component";
+import {LinageComponent} from "./additional-components/linage-component/linage-component";
 
 @NgModule({
     imports: [
@@ -45,8 +46,8 @@ import {OverviewComponent} from "./additional-components/overview-component/over
         MatTabsModule,
         MonacoEditorModule,
     ],
-    exports: [DatasetComponent, MetadataComponent, DataComponent, OverviewComponent],
-    declarations: [DatasetComponent, MetadataComponent, DataComponent, OverviewComponent],
+    exports: [DatasetComponent, MetadataComponent, DataComponent, OverviewComponent, LinageComponent],
+    declarations: [DatasetComponent, MetadataComponent, DataComponent, OverviewComponent, LinageComponent],
 })
 export class DatasetModule {
     public static forRoot(): ModuleWithProviders<DatasetModule> {

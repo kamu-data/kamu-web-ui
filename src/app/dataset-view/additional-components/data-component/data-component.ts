100e352d0b80f1b79686b54b487c1afdea32ee8e
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2022-02-25 17:58:44 -0800

    Fix Ctrl+Enter handling for SQL execution

--- src/app/dataset-view/additional-components/data-component/data-component.ts
+++ src/app/dataset-view/additional-components/data-component/data-component.ts
@@ -36,24 +36,48 @@ export class DataComponent implements OnInit {
     public sqlEditorOptions = {
         theme: "vs",
         language: "sql",
+        minimap: {
+            enabled: false,
+        },
     };
     public savedQueries = DataTabValues.savedQueries;
-    public sqlRequestCode: string = `select * from `;
-    @HostListener("document:keydown", ["$event"])
-    private checkCTRLEvent(event: KeyboardEvent): void {
-        if (event.ctrlKey && event.key === "Enter") {
-            this.onRunSQLRequest();
-        }
-    }
+    public sqlRequestCode: string = `select\n  *\nfrom `;
+
     public onSelectDataset(id: string): void {
         this.onSelectDatasetEmit.emit(id);
     }
+
     public onRunSQLRequest(sqlRequestCode?: string): void {
         this.onRunSQLRequestEmit.emit(sqlRequestCode || this.sqlRequestCode);
     }
+
     public ngOnInit(): void {
         if (this.datasetName) {
             this.sqlRequestCode += `'${this.datasetName.name}'`;
         }
+    }
+
+    onInitEditor(editor: any): void {
+        let self = this;
+        let runQueryFn = function (_editor: any) {
+            self.onRunSQLRequest();
+        };
+        editor.addAction({
+            // An unique identifier of the contributed action.
+            id: "run-sql",
+            // A label of the action that will be presented to the user.
+            label: "Run SQL",
+            // An optional array of keybindings for the action.
+            keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter],
+            // A precondition for this action.
+            precondition: null,
+            // A rule to evaluate on top of the precondition in order to dispatch the keybindings.
+            keybindingContext: null,
+            contextMenuGroupId: "navigation",
+            contextMenuOrder: 1.5,
+            // Method that will be executed when the action is triggered.
+            // @param editor The editor instance is passed in as a convenience
+            run: runQueryFn,
+        });
     }
 }

6539296e2e51689e12a2c1c2d331f91ce7514c68
Sergei Zaychenko <szaychenko@kamu.dev>
2022-08-19 01:49:26 -0700

    Removed uunused env fields

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,6 @@
 export const environment = {
     production: false,
     github_client_id: "361a3b4fda86d0234d2f", // your Client ID from GitHub
-    redirect_uri: "http://127.0.0.1:4200/v/github_callback", // authentication url
-    gatekeeper: "https://github.com/login/oauth/authorize", // url from gatekeeper
 };
 
 /*

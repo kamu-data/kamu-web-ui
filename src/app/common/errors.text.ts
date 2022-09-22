5b7b647163a39cca1eb4c2c1db5ca7e81b987657
Dmitriy Borzenko <borzenko@dnt-lab.com>
2022-09-22 18:41:48 +0300

    Minor changes

--- src/app/common/errors.text.ts
+++ src/app/common/errors.text.ts
@@ -1,12 +1,12 @@
 export class ErrorTexts {
-    public static readonly ERROR_TITLE_REQUEST_FAILED = "Request was failed.";
+    public static readonly ERROR_TITLE_REQUEST_FAILED = "Request failed.";
     public static readonly ERROR_NETWORK_DESCRIPTION =
-        "Check the internet connection.";
+        "Check the Internet connection.";
     public static readonly ERROR_DATASET_NOT_FOUND = "dataset not found.";
     public static readonly ERROR_BAD_SQL_QUERY = "Incorrect SQL query.";
     public static readonly ERROR_SERVER_BAD_SQL_QUERY = "DataFusion error";
     public static readonly ERROR_TECHNICAL_SUPPORT =
-        "Operation failed. If this happens again, please, contact technical support";
+        "Operation failed. If this happens again, please contact Kamu technical support.";
 }
 
 export const DictionaryErrors = [

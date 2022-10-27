b7adb912de48eb2f753482a51b8ee86e1e84e2b7
Sergei Zaychenko <zaychenko.sergei@gmail.com>
2022-10-27 09:41:47 -0700

    Restored correct run-time configuration of GraphQL server URL (#55)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,4 @@ export const environment = {
     production: true,
     github_client_id: "361a3b4fda86d0234d2f", // your Client ID from GitHub
     delay_http_request_ms: 0,
-    api_server_gql_url: "http://localhost:8080/graphql",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,6 @@ export const environment = {
     production: false,
     github_client_id: "361a3b4fda86d0234d2f", // your Client ID from GitHub,
     delay_http_request_ms: 500,
-    api_server_gql_url: "http://localhost:8080/graphql",
 };
 
 /*

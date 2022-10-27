ce509d1eaa409a1d97b0b5dac0896ed867db9296
Sergei Zaychenko <szaychenko@kamu.dev>
2022-10-27 07:32:46 -0700

    Restored correct run-time configuration of GraphQL URL

--- src/app/app-config.service.ts
+++ src/app/app-config.service.ts
@@ -1,23 +1,35 @@
 import { Injectable } from "@angular/core";
-import { environment } from "src/environments/environment";
+
+interface AppConfig {
+    apiServerGqlUrl: string;
+}
 
 @Injectable({
     providedIn: "root",
 })
 export class AppConfigService {
+    private appConfig?: AppConfig;
 
-    private readonly _apiServerGqlUrl: string;
+    get apiServerGqlUrl(): string {
+        if (!this.appConfig) {
+            this.appConfig = AppConfigService.loadAppConfig();
+        }
 
-    constructor() {
-        this._apiServerGqlUrl = this.toRemoteURL(environment.api_server_gql_url);
+        return this.appConfig.apiServerGqlUrl;
     }
 
-    get apiServerGqlUrl() {
-        return this._apiServerGqlUrl;
+    private static loadAppConfig(): AppConfig {
+        const request = new XMLHttpRequest();
+        request.open('GET', "/assets/runtime-config.json", false);  
+        request.send(null);
+        const data: AppConfig = JSON.parse(request.responseText) as AppConfig;
+        return {
+            apiServerGqlUrl: AppConfigService.toRemoteURL(data.apiServerGqlUrl)
+        };
     }
 
     // If loopback or any address is used - replace hostname with hostname from the browser
-    private toRemoteURL(url: string): string {
+    private static toRemoteURL(url: string): string {
         const turl = new URL(url);
         if (
             ["localhost", "127.0.0.1", "0.0.0.0", "[::]"].includes(

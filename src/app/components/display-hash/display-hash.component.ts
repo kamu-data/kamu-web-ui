027c97ca7f4c598981ac1eab3b077a1de376d4ae
Dmitriy Borzenko <borzenko@dnt-lab.com>
2022-12-13 11:26:45 +0200

    Changed input properties for DisplayHash component

--- src/app/components/display-hash/display-hash.component.ts
+++ src/app/components/display-hash/display-hash.component.ts
@@ -11,9 +11,9 @@ import { Clipboard } from "@angular/cdk/clipboard";
 export class DisplayHashComponent {
     @Input() public value: string;
     @Input() public datasetInfo: DatasetInfo;
-    @Input() public showCopyButton?: boolean = false;
-    @Input() public class?: string;
-    @Input() public isNavigable?: boolean = true;
+    @Input() public showCopyButton = false;
+    @Input() public class = "mr-4 hashBlock";
+    @Input() public isNavigable = true;
 
     constructor(
         private navigationService: NavigationService,
@@ -33,6 +33,8 @@ export class DisplayHashComponent {
     }
 
     public copyToClipboard(event: Event, text: string): void {
+        const displayInlineBlock = "inline-block";
+        const displayNone = "none";
         this.clipboard.copy(text);
         if (event.currentTarget !== null) {
             const currentElement: HTMLButtonElement =
@@ -40,13 +42,13 @@ export class DisplayHashComponent {
             const currentElementChildren: HTMLCollectionOf<HTMLElement> =
                 currentElement.children as HTMLCollectionOf<HTMLElement>;
             setTimeout(() => {
-                currentElementChildren[0].style.display = "inline-block";
-                currentElementChildren[1].style.display = "none";
+                currentElementChildren[0].style.display = displayInlineBlock;
+                currentElementChildren[1].style.display = displayNone;
                 currentElement.classList.remove("clipboard-btn--success");
             }, 2000);
 
-            currentElementChildren[0].style.display = "none";
-            currentElementChildren[1].style.display = "inline-block";
+            currentElementChildren[0].style.display = displayNone;
+            currentElementChildren[1].style.display = displayInlineBlock;
             currentElement.classList.add("clipboard-btn--success");
         }
     }

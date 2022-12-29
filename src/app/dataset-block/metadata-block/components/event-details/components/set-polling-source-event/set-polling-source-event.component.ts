a6a381855fb2bbde89a574b128125d5f3085cf00
Dmitriy Borzenko <borzenko@dnt-lab.com>
2022-12-29 19:05:39 +0200

    Added template for SetPollingSource event

--- src/app/dataset-block/metadata-block/components/event-details/components/set-polling-source-event/set-polling-source-event.component.ts
+++ src/app/dataset-block/metadata-block/components/event-details/components/set-polling-source-event/set-polling-source-event.component.ts
@@ -1,6 +1,12 @@
-import { SetPollingSource } from './../../../../../../api/kamu.graphql.interface';
+import {
+    FetchStepFilesGlob,
+    FetchStepUrl,
+    MergeStrategyLedger,
+    ReadStepCsv,
+    SetPollingSource,
+} from "./../../../../../../api/kamu.graphql.interface";
 import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
-import { DatasetInfo } from 'src/app/interface/navigation.interface';
+import { DatasetInfo } from "src/app/interface/navigation.interface";
 
 @Component({
     selector: "app-set-polling-source-event",
@@ -9,6 +15,38 @@ import { DatasetInfo } from 'src/app/interface/navigation.interface';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SetPollingSourceEventComponent {
-  @Input() public event: SetPollingSource;
-  @Input() public datasetInfo: DatasetInfo;
+    @Input() public event: SetPollingSource;
+    @Input() public datasetInfo: DatasetInfo;
+
+    public get fetchStepUrl(): FetchStepUrl {
+        return this.event.fetch as FetchStepUrl;
+    }
+
+    public get isFetchStepUrl(): boolean {
+        return this.event.fetch.__typename === "FetchStepUrl";
+    }
+
+    public get fetchStepFilesGlob(): FetchStepFilesGlob {
+        return this.event.fetch as FetchStepFilesGlob;
+    }
+
+    public get isFetchStepFilesGlob(): boolean {
+        return this.event.fetch.__typename === "FetchStepFilesGlob";
+    }
+
+    public get readStepCsv(): ReadStepCsv {
+        return this.event.read as ReadStepCsv;
+    }
+
+    public get isReadStepCsv(): boolean {
+        return this.event.read.__typename === "ReadStepCsv";
+    }
+
+    public get mergeStrategyLedger(): MergeStrategyLedger {
+        return this.event.merge as MergeStrategyLedger;
+    }
+
+    public get isMergeStrategyLedger(): boolean {
+        return this.event.merge.__typename === "MergeStrategyLedger";
+    }
 }

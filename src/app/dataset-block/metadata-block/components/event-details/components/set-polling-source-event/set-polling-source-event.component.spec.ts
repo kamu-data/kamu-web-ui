37d4139aff75ff671dbd78d6a78c4cfe5567c413
Dmitriy Borzenko <borzenko@dnt-lab.com>
2023-01-13 09:53:08 +0200

    Fixed warning in unit tests

--- src/app/dataset-block/metadata-block/components/event-details/components/set-polling-source-event/set-polling-source-event.component.spec.ts
+++ src/app/dataset-block/metadata-block/components/event-details/components/set-polling-source-event/set-polling-source-event.component.spec.ts
@@ -1,8 +1,16 @@
+import { CardsPropertyComponent } from "./../common/cards-property/cards-property.component";
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { mockSetPollingSourceEvent } from "../../mock.events";
 
 import { SetPollingSourceEventComponent } from "./set-polling-source-event.component";
+import { EditorPropertyComponent } from "../common/editor-property/editor-property.component";
+import { EnginePropertyComponent } from "../common/engine-property/engine-property.component";
+import { EnvVariablesPropertyComponent } from "../common/env-variables-property/env-variables-property.component";
+import { LinkPropertyComponent } from "../common/link-property/link-property.component";
+import { SchemaPropertyComponent } from "../common/schema-property/schema-property.component";
+import { SeparatorPropertyComponent } from "../common/separator-property/separator-property.component";
+import { SimplePropertyComponent } from "../common/simple-property/simple-property.component";
 
 describe("SetPollingSourceEventComponent", () => {
     let component: SetPollingSourceEventComponent;
@@ -10,7 +18,17 @@ describe("SetPollingSourceEventComponent", () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [SetPollingSourceEventComponent],
+            declarations: [
+                SetPollingSourceEventComponent,
+                SimplePropertyComponent,
+                SeparatorPropertyComponent,
+                LinkPropertyComponent,
+                EnginePropertyComponent,
+                EditorPropertyComponent,
+                SchemaPropertyComponent,
+                EnvVariablesPropertyComponent,
+                CardsPropertyComponent,
+            ],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
 

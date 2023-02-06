049f2c7f7609530075b05314894964880950255e
Dmitriy Borzenko <borzenko@dnt-lab.com>
2023-02-07 00:23:17 +0200

    Added unit tests for YamlEventViewer component.

--- src/app/dataset-block/metadata-block/components/event-details/components/common/yaml-event-viewer/yaml-event-viewer.component.spec.ts
+++ src/app/dataset-block/metadata-block/components/event-details/components/common/yaml-event-viewer/yaml-event-viewer.component.spec.ts
@@ -1,11 +1,21 @@
-import { SetTransform } from "./../../../../../../../api/kamu.graphql.interface";
+import {
+    SetAttachments,
+    SetInfo,
+    SetLicense,
+    SetTransform,
+} from "./../../../../../../../api/kamu.graphql.interface";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
-import { mockSetTransform } from "./../../../mock.events";
+import {
+    mockSetAttachments,
+    mockSetInfo,
+    mockSetLicense,
+    mockSetTransform,
+} from "./../../../mock.events";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { YamlEventViewerComponent } from "./yaml-event-viewer.component";
 
-describe("YamlEventViewerComponent", () => {
+describe("YamlEventViewerComponent with SetTransform", () => {
     let component: YamlEventViewerComponent<SetTransform>;
     let fixture: ComponentFixture<YamlEventViewerComponent<SetTransform>>;
 
@@ -23,7 +33,76 @@ describe("YamlEventViewerComponent", () => {
         fixture.detectChanges();
     });
 
-    it("should create", () => {
+    it("should create component with SetTransform", () => {
+        expect(component).toBeTruthy();
+    });
+});
+
+describe("YamlEventViewerComponent with SetLicense", () => {
+    let component: YamlEventViewerComponent<SetLicense>;
+    let fixture: ComponentFixture<YamlEventViewerComponent<SetLicense>>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [YamlEventViewerComponent],
+            schemas: [NO_ERRORS_SCHEMA],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent<YamlEventViewerComponent<SetLicense>>(
+            YamlEventViewerComponent,
+        );
+        component = fixture.componentInstance;
+        component.event = mockSetLicense;
+        fixture.detectChanges();
+    });
+
+    it("should create component with SetLicense", () => {
+        expect(component).toBeTruthy();
+    });
+});
+
+describe("YamlEventViewerComponent with SetInfo", () => {
+    let component: YamlEventViewerComponent<SetInfo>;
+    let fixture: ComponentFixture<YamlEventViewerComponent<SetInfo>>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [YamlEventViewerComponent],
+            schemas: [NO_ERRORS_SCHEMA],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent<YamlEventViewerComponent<SetInfo>>(
+            YamlEventViewerComponent,
+        );
+        component = fixture.componentInstance;
+        component.event = mockSetInfo;
+        fixture.detectChanges();
+    });
+
+    it("should create component with SetInfo", () => {
+        expect(component).toBeTruthy();
+    });
+});
+
+describe("YamlEventViewerComponent with SetAttachments", () => {
+    let component: YamlEventViewerComponent<SetAttachments>;
+    let fixture: ComponentFixture<YamlEventViewerComponent<SetAttachments>>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [YamlEventViewerComponent],
+            schemas: [NO_ERRORS_SCHEMA],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent<
+            YamlEventViewerComponent<SetAttachments>
+        >(YamlEventViewerComponent);
+        component = fixture.componentInstance;
+        component.event = mockSetAttachments;
+        fixture.detectChanges();
+    });
+
+    it("should create component with SetAttachments", () => {
         expect(component).toBeTruthy();
     });
 });

29f28b3d5efc313fc433165741cd3c95e40720d3
Sergei Zaychenko <szaychenko@kamu.dev>
2023-09-28 02:55:22 -0700

    Not wrapping fatal GraphQL engine errors into application exceptions

--- src/app/common/errors.text.ts
+++ src/app/common/errors.text.ts
@@ -1,7 +1,7 @@
 export class ErrorTexts {
     public static readonly ERROR_TITLE_REQUEST_FAILED = "Request failed.";
     public static readonly ERROR_NETWORK_DESCRIPTION = "Check the Internet connection.";
-    public static readonly ERROR_EXECUTING_SQL_QUERY = "Executing SQL query failed.";
+    public static readonly ERROR_EXECUTING_SQL_QUERY = "Executing SQL query failed";
     public static readonly ERROR_INVALID_SQL_QUERY = "Invalid SQL query";
     public static readonly ERROR_TECHNICAL_SUPPORT =
         "Operation failed. If this happens again, please contact Kamu technical support.";

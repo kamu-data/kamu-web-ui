470849060c8c8e4eeac2630a9b608ef41108fef8
Dmitriy Borzenko <borzenko@dnt-lab.com>
2023-10-05 14:14:57 +0300

    Fixed some files with ViewEncapsulation.None.

--- src/app/dataset-view/additional-components/data-component/data.component.ts
+++ src/app/dataset-view/additional-components/data-component/data.component.ts
@@ -16,6 +16,7 @@ import { Observable, map, tap } from "rxjs";
 @Component({
     selector: "app-data",
     templateUrl: "./data.component.html",
+    styleUrls: ["./data.component.scss"],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DataComponent extends BaseComponent implements OnInit {

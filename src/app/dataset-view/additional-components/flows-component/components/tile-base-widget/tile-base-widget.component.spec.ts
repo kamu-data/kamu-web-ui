ff31dfa89f1cc846c392c1a37d8e6a0062d57ab6
Dmitriy Borzenko <borzenko@dnt-lab.com>
2024-01-31 20:43:37 +0200

    Fixed prettier warning

--- src/app/dataset-view/additional-components/flows-component/components/tile-base-widget/tile-base-widget.component.spec.ts
+++ src/app/dataset-view/additional-components/flows-component/components/tile-base-widget/tile-base-widget.component.spec.ts
@@ -1,23 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { TileBaseWidgetComponent } from "./tile-base-widget.component";
 
-import { TileBaseWidgetComponent } from './tile-base-widget.component';
+describe("TileBaseWidgetComponent", () => {
+    let component: TileBaseWidgetComponent;
+    let fixture: ComponentFixture<TileBaseWidgetComponent>;
 
-describe('TileBaseWidgetComponent', () => {
-  let component: TileBaseWidgetComponent;
-  let fixture: ComponentFixture<TileBaseWidgetComponent>;
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [TileBaseWidgetComponent],
+        }).compileComponents();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TileBaseWidgetComponent ]
-    })
-    .compileComponents();
+        fixture = TestBed.createComponent(TileBaseWidgetComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-    fixture = TestBed.createComponent(TileBaseWidgetComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });

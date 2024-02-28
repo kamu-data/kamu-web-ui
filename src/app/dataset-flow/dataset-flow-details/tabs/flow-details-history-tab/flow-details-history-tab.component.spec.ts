e8355ea33b28a52faf2b4c62b1cb6d7e158d05c5
Dmitriy Borzenko <borzenko@dnt-lab.com>
2024-02-28 14:24:49 +0200

    Fixed unit test

--- src/app/dataset-flow/dataset-flow-details/tabs/flow-details-history-tab/flow-details-history-tab.component.spec.ts
+++ src/app/dataset-flow/dataset-flow-details/tabs/flow-details-history-tab/flow-details-history-tab.component.spec.ts
@@ -2,6 +2,9 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { FlowDetailsHistoryTabComponent } from "./flow-details-history-tab.component";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatIconModule } from "@angular/material/icon";
+import { Apollo, ApolloModule } from "apollo-angular";
+import { ApolloTestingModule } from "apollo-angular/testing";
+import { SharedTestModule } from "src/app/common/shared-test.module";
 
 describe("FlowDetailsHistoryTabComponent", () => {
     let component: FlowDetailsHistoryTabComponent;
@@ -10,11 +13,13 @@ describe("FlowDetailsHistoryTabComponent", () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [FlowDetailsHistoryTabComponent],
-            imports: [MatDividerModule, MatIconModule],
+            imports: [MatDividerModule, MatIconModule, ApolloModule, ApolloTestingModule, SharedTestModule],
+            providers: [Apollo],
         }).compileComponents();
 
         fixture = TestBed.createComponent(FlowDetailsHistoryTabComponent);
         component = fixture.componentInstance;
+        component.flowHistory = [];
         fixture.detectChanges();
     });
 

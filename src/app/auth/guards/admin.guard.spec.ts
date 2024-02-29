fa82c8d985eb6f404f62b0b6461e31bd2fc1647d
Dmitriy Borzenko <borzenko@dnt-lab.com>
2024-02-29 13:15:38 +0200

    Fixed unit test regression

--- src/app/auth/guards/admin.guard.spec.ts
+++ src/app/auth/guards/admin.guard.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from "@angular/core/testing";
-
 import { AdminGuard } from "./admin.guard";
+import { Apollo, ApolloModule } from "apollo-angular";
+import { ApolloTestingModule } from "apollo-angular/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe("AdminGuard", () => {
     let guard: AdminGuard;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [Apollo],
+            imports: [ApolloModule, ApolloTestingModule, HttpClientTestingModule],
+        });
         guard = TestBed.inject(AdminGuard);
     });
 

d11aad247da3aef298a79b0b5dffc7785d018d56
Dmitriy Borzenko <borzenko@dnt-lab.com>
2024-03-01 15:44:02 +0200

    Updated graphql fragments

--- src/app/api/kamu.graphql.interface.ts
+++ src/app/api/kamu.graphql.interface.ts
@@ -838,18 +838,11 @@ export type FlowEventInitiated = FlowEvent & {
     trigger: FlowTrigger;
 };
 
-export type FlowEventQueued = FlowEvent & {
-    __typename?: "FlowEventQueued";
-    activateAt: Scalars["DateTime"];
-    eventId: Scalars["EventID"];
-    eventTime: Scalars["DateTime"];
-};
-
 export type FlowEventStartConditionUpdated = FlowEvent & {
     __typename?: "FlowEventStartConditionUpdated";
     eventId: Scalars["EventID"];
     eventTime: Scalars["DateTime"];
-    startConditionKind?: Maybe<FlowStartConditionKind>;
+    startConditionKind: FlowStartConditionKind;
 };
 
 export type FlowEventTaskChanged = FlowEvent & {
@@ -889,12 +882,6 @@ export type FlowNotFound = CancelScheduledTasksResult &
         message: Scalars["String"];
     };
 
-export type FlowNotScheduled = CancelScheduledTasksResult & {
-    __typename?: "FlowNotScheduled";
-    flowId: Scalars["FlowID"];
-    message: Scalars["String"];
-};
-
 export enum FlowOutcome {
     Aborted = "ABORTED",
     Cancelled = "CANCELLED",
@@ -902,7 +889,11 @@ export enum FlowOutcome {
     Success = "SUCCESS",
 }
 
-export type FlowStartCondition = FlowStartConditionBatching | FlowStartConditionThrottling;
+export type FlowStartCondition =
+    | FlowStartConditionBatching
+    | FlowStartConditionExecutor
+    | FlowStartConditionSchedule
+    | FlowStartConditionThrottling;
 
 export type FlowStartConditionBatching = {
     __typename?: "FlowStartConditionBatching";
@@ -912,28 +903,40 @@ export type FlowStartConditionBatching = {
     watermarkModified: Scalars["Boolean"];
 };
 
+export type FlowStartConditionExecutor = {
+    __typename?: "FlowStartConditionExecutor";
+    taskId: Scalars["TaskID"];
+};
+
 export enum FlowStartConditionKind {
     Batching = "BATCHING",
+    Executor = "EXECUTOR",
+    Schedule = "SCHEDULE",
     Throttling = "THROTTLING",
 }
+
+export type FlowStartConditionSchedule = {
+    __typename?: "FlowStartConditionSchedule";
+    wakeUpAt: Scalars["DateTime"];
+};
 
 export type FlowStartConditionThrottling = {
     __typename?: "FlowStartConditionThrottling";
     intervalSec: Scalars["Int"];
+    shiftedFrom: Scalars["DateTime"];
+    wakeUpAt: Scalars["DateTime"];
 };
 
 export enum FlowStatus {
     Finished = "FINISHED",
-    Queued = "QUEUED",
     Running = "RUNNING",
-    Scheduled = "SCHEDULED",
     Waiting = "WAITING",
 }
 
 export type FlowTimingRecords = {
     __typename?: "FlowTimingRecords";
-    /** Planned activation time (at least, Queued state) */
-    activateAt?: Maybe<Scalars["DateTime"]>;
+    /** Recorded time of last task scheduling */
+    awaitingExecutorSince?: Maybe<Scalars["DateTime"]>;
     /**
      * Recorded time of finish (successful or failed after retry) or abortion
      * (Finished state seen at least once)
@@ -1954,10 +1957,9 @@ export type GetFlowByIdQuery = {
                                       | ({
                                             __typename?: "FlowEventInitiated";
                                         } & FlowHistoryData_FlowEventInitiated_Fragment)
-                                      | ({ __typename?: "FlowEventQueued" } & FlowHistoryData_FlowEventQueued_Fragment)
                                       | ({
-                                            __typename?: "FlowEventStartConditionDefined";
-                                        } & FlowHistoryData_FlowEventStartConditionDefined_Fragment)
+                                            __typename?: "FlowEventStartConditionUpdated";
+                                        } & FlowHistoryData_FlowEventStartConditionUpdated_Fragment)
                                       | ({
                                             __typename?: "FlowEventTaskChanged";
                                         } & FlowHistoryData_FlowEventTaskChanged_Fragment)
@@ -2128,7 +2130,7 @@ export type FlowSummaryDataFragment = {
     initiator?: ({ __typename?: "Account" } & AccountFragment) | null;
     timing: {
         __typename?: "FlowTimingRecords";
-        activateAt?: string | null;
+        awaitingExecutorSince?: string | null;
         runningSince?: string | null;
         finishedAt?: string | null;
     };
@@ -2144,6 +2146,8 @@ export type FlowSummaryDataFragment = {
                   maxBatchingInterval: { __typename?: "TimeDelta" } & TimeDeltaDataFragment;
               };
           }
+        | { __typename: "FlowStartConditionExecutor"; taskId: string }
+        | { __typename: "FlowStartConditionSchedule"; wakeUpAt: string }
         | { __typename: "FlowStartConditionThrottling"; intervalSec: number }
         | null;
 };
@@ -2169,20 +2173,11 @@ type FlowHistoryData_FlowEventInitiated_Fragment = {
         | { __typename: "FlowTriggerPush"; dummy: boolean };
 };
 
-type FlowHistoryData_FlowEventQueued_Fragment = {
-    __typename: "FlowEventQueued";
+type FlowHistoryData_FlowEventStartConditionUpdated_Fragment = {
+    __typename: "FlowEventStartConditionUpdated";
     eventId: string;
     eventTime: string;
-    activateAt: string;
-};
-
-type FlowHistoryData_FlowEventStartConditionDefined_Fragment = {
-    __typename: "FlowEventStartConditionDefined";
-    eventId: string;
-    eventTime: string;
-    startCondition:
-        | { __typename: "FlowStartConditionBatching"; thresholdNewRecords: number }
-        | { __typename: "FlowStartConditionThrottling"; intervalSec: number };
+    startConditionKind: FlowStartConditionKind;
 };
 
 type FlowHistoryData_FlowEventTaskChanged_Fragment = {
@@ -2207,8 +2202,7 @@ type FlowHistoryData_FlowEventTriggerAdded_Fragment = {
 export type FlowHistoryDataFragment =
     | FlowHistoryData_FlowEventAborted_Fragment
     | FlowHistoryData_FlowEventInitiated_Fragment
-    | FlowHistoryData_FlowEventQueued_Fragment
-    | FlowHistoryData_FlowEventStartConditionDefined_Fragment
+    | FlowHistoryData_FlowEventStartConditionUpdated_Fragment
     | FlowHistoryData_FlowEventTaskChanged_Fragment
     | FlowHistoryData_FlowEventTriggerAdded_Fragment;
 
@@ -3022,7 +3016,7 @@ export const FlowSummaryDataFragmentDoc = gql`
         }
         outcome
         timing {
-            activateAt
+            awaitingExecutorSince
             runningSince
             finishedAt
         }
@@ -3041,6 +3035,12 @@ export const FlowSummaryDataFragmentDoc = gql`
                 batchingDeadline
                 accumulatedRecordsCount
                 watermarkModified
+            }
+            ... on FlowStartConditionSchedule {
+                wakeUpAt
+            }
+            ... on FlowStartConditionExecutor {
+                taskId
             }
         }
     }
@@ -3103,24 +3103,10 @@ export const FlowHistoryDataFragmentDoc = gql`
                 }
             }
         }
-        ... on FlowEventQueued {
-            __typename
+        ... on FlowEventStartConditionUpdated {
             eventId
             eventTime
-            activateAt
-        }
-        ... on FlowEventStartConditionDefined {
-            eventId
-            eventTime
-            startCondition {
-                __typename
-                ... on FlowStartConditionThrottling {
-                    intervalSec
-                }
-                ... on FlowStartConditionBatching {
-                    thresholdNewRecords
-                }
-            }
+            startConditionKind
         }
         ... on FlowEventTaskChanged {
             __typename

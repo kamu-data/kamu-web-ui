68c1463575a9fb5038624863f31c50b509754a9f
Sergei Zaychenko <szaychenko@kamu.dev>
2024-05-04 08:17:44 -0700

    AccountID is no longer artifficial. (#308)
    Removed caching complications for account, default rules work fine.

--- src/app/common/data.helpers.spec.ts
+++ src/app/common/data.helpers.spec.ts
@@ -172,7 +172,7 @@ it("should check description for SetTransform block", () => {
                         name: "quebec.case-details",
                         owner: {
                             __typename: "Account",
-                            id: "1",
+                            id: "did:odf:fed016b61ed2ab1b63a006b61ed2ab1b63a00b016d65607000000e0821aafbf163e6f",
                             accountName: "kamu",
                         },
                         alias: "kamu/quebec.case-details",
--- src/app/dataset-view/additional-components/metadata-component/components/set-transform/mock.data.ts
+++ src/app/dataset-view/additional-components/metadata-component/components/set-transform/mock.data.ts
@@ -61,7 +61,7 @@ export const mockGetDatasetSchemaQuery: GetDatasetSchemaQuery = {
             name: "account.tokens.portfolio.usd",
             owner: {
                 __typename: "Account",
-                id: "1",
+                id: "did:odf:fed016b61ed2ab1b63a006b61ed2ab1b63a00b016d65607000000e0821aafbf163e6f",
                 accountName: "kamu",
             },
             alias: "kamu/account.tokens.portfolio.usd",

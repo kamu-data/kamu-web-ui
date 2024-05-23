f4187da72b92deb72615152754f61ca97483f547
Sergei Zaychenko <szaychenko@kamu.dev>
2024-05-23 04:19:24 -0700

    Dirty draft of upload code

--- src/app/dataset-view/additional-components/overview-component/components/add-data-modal/add-data-modal.component.ts
+++ src/app/dataset-view/additional-components/overview-component/components/add-data-modal/add-data-modal.component.ts
@@ -3,6 +3,9 @@ import { NgbActiveModal, NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstra
 import { BaseComponent } from "src/app/common/base.component";
 import { FileFromUrlModalComponent } from "../file-from-url-modal/file-from-url-modal.component";
 import { MaybeNull } from "src/app/common/app.types";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { LocalStorageService } from "src/app/services/local-storage.service";
 
 @Component({
     selector: "app-add-data-modal",
@@ -14,6 +17,8 @@ export class AddDataModalComponent extends BaseComponent implements OnInit {
     constructor(
         public activeModal: NgbActiveModal,
         private modalService: NgbModal,
+        private localStorageService: LocalStorageService,
+        private http: HttpClient,
     ) {
         super();
     }
@@ -27,7 +32,51 @@ export class AddDataModalComponent extends BaseComponent implements OnInit {
                 resolve("");
             } else {
                 const file: File = input.files[0];
-                console.log("file", file);
+
+                interface UploadFormField {
+                    name: string;
+                    value: string;
+                }
+
+                interface UploadGetResponse {
+                    uploadUrl: string;
+                    method: "POST" | "PUT";
+                    fields: UploadFormField[];
+                }
+
+                const getHeaders = { Authorization: `Bearer ${this.localStorageService.accessToken}` };
+                const uploadGet$: Observable<UploadGetResponse> = this.http.get<UploadGetResponse>(
+                    "http://localhost:8080/platform/file/upload?file_name=" + file.name,
+                    { headers: getHeaders },
+                );
+
+                uploadGet$.subscribe((response: UploadGetResponse) => {
+                    const formData = new FormData();
+                    formData.append("thumbnail", file);
+
+                    // TODO: use fields
+                    let uploadHeaders = new HttpHeaders();
+                    uploadHeaders = uploadHeaders.append(
+                        "Authorization",
+                        `Bearer ${this.localStorageService.accessToken}`,
+                    );
+                    response.fields.forEach((field: UploadFormField) => {
+                        uploadHeaders = uploadHeaders.append(field.name, field.value);
+                    });
+
+                    let upload$: Observable<object>;
+                    switch (response.method) {
+                        case "POST":
+                            upload$ = this.http.post(response.uploadUrl, formData, { headers: uploadHeaders });
+                            break;
+                        case "PUT":
+                            upload$ = this.http.put(response.uploadUrl, formData, { headers: uploadHeaders });
+                            break;
+                    }
+                    upload$.subscribe(() => {
+                        console.log("Upload done");
+                    });
+                });
             }
         });
     }

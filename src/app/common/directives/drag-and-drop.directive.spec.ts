d1cd7d347fd309034e1b196f8530ab8e470173f4
Dmitriy Borzenko <borzenko@dnt-lab.com>
2024-06-14 19:27:28 +0300

    Added more tests

--- src/app/common/directives/drag-and-drop.directive.spec.ts
+++ src/app/common/directives/drag-and-drop.directive.spec.ts
@@ -5,4 +5,22 @@ describe("DragAndDropDirective", () => {
         const directive = new DragAndDropDirective();
         expect(directive).toBeTruthy();
     });
+
+    it("should drop event", () => {
+        const directive = new DragAndDropDirective();
+        const fileDroppedSpy = spyOn(directive.fileDropped, "emit");
+        const file = new File([""], "dummy.jpg");
+        const fileDropEvent = {
+            preventDefault: () => {},
+            stopPropagation: () => {},
+            dataTransfer: {
+                files: [file],
+            },
+        } as unknown as DragEvent;
+        directive.onDragLeave(fileDropEvent);
+        directive.onDragOver(fileDropEvent);
+        directive.onDrop(fileDropEvent);
+
+        expect(fileDroppedSpy).toHaveBeenCalledTimes(1);
+    });
 });
--- src/app/dataset-view/additional-components/overview-component/components/add-data-modal/add-data-modal.component.spec.ts
+++ src/app/dataset-view/additional-components/overview-component/components/add-data-modal/add-data-modal.component.spec.ts
@@ -2,14 +2,23 @@ import { SharedTestModule } from "src/app/common/shared-test.module";
 import { Apollo } from "apollo-angular";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { AddDataModalComponent } from "./add-data-modal.component";
-import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { NgbActiveModal, NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { HttpClientModule } from "@angular/common/http";
 import { MatIconModule } from "@angular/material/icon";
 import { MatDividerModule } from "@angular/material/divider";
+import { mockDatasetBasicsRootFragment } from "src/app/search/mock.data";
+import { FileFromUrlModalComponent } from "../file-from-url-modal/file-from-url-modal.component";
+import { FileUploadService } from "src/app/services/file-upload.service";
+import { mockFile } from "src/app/api/mock/upload-file.mock";
+import { ModalService } from "src/app/components/modal/modal.service";
+import { of } from "rxjs";
 
 describe("AddDataModalComponent", () => {
     let component: AddDataModalComponent;
     let fixture: ComponentFixture<AddDataModalComponent>;
+    let ngbModalService: NgbModal;
+    let fileUploadService: FileUploadService;
+    let modalService: ModalService;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -20,10 +29,50 @@ describe("AddDataModalComponent", () => {
 
         fixture = TestBed.createComponent(AddDataModalComponent);
         component = fixture.componentInstance;
+        ngbModalService = TestBed.inject(NgbModal);
+        fileUploadService = TestBed.inject(FileUploadService);
+        modalService = TestBed.inject(ModalService);
+        component.datasetBasics = mockDatasetBasicsRootFragment;
         fixture.detectChanges();
     });
 
     it("should create", () => {
         expect(component).toBeTruthy();
+    });
+
+    it("should check open modal with file url", () => {
+        const ngbModalServiceOpenSpy = spyOn(ngbModalService, "open");
+        component.onAddUrl();
+        expect(ngbModalServiceOpenSpy).toHaveBeenCalledWith(FileFromUrlModalComponent);
+    });
+
+    it("should check if file selected and file size < 50MB", () => {
+        const uploadFileSpy = spyOn(fileUploadService, "uploadFile").and.callFake(() => of({}));
+        const event = {
+            target: {
+                files: [mockFile],
+            },
+        } as unknown as Event;
+        component.onFileSelected(event);
+        expect(uploadFileSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it("should check if file selected and file size > 50MB", () => {
+        const modalServiceSpy = spyOn(modalService, "warning").and.callFake((options) => {
+            options.handler?.call(undefined, false);
+            return Promise.resolve("");
+        });
+        const largeFile = {
+            name: "data.csv",
+            size: Math.pow(10, 8),
+            type: "text/csv",
+        } as File;
+        const event = {
+            target: {
+                files: [largeFile],
+            },
+        } as unknown as Event;
+        component.onFileSelected(event);
+        expect(modalServiceSpy).toHaveBeenCalledTimes(1);
     });
 });

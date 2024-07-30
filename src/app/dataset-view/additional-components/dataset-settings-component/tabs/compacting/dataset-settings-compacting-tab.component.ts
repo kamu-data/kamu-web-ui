bf4df2ceeb3deb9b52f08469446c49e4a47ee22e
Dmitriy Borzenko <borzenko@dnt-lab.com>
2024-07-30 18:34:11 +0300

    Modified template for hard compacting

--- src/app/dataset-view/additional-components/dataset-settings-component/tabs/compacting/dataset-settings-compacting-tab.component.ts
+++ src/app/dataset-view/additional-components/dataset-settings-component/tabs/compacting/dataset-settings-compacting-tab.component.ts
@@ -1,12 +1,12 @@
 import { NavigationService } from "./../../../../../services/navigation.service";
-import { ChangeDetectionStrategy, Component, Input, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 import { AbstractControl, FormBuilder, Validators } from "@angular/forms";
 import { RxwebValidators } from "@rxweb/reactive-form-validators";
-import { CompactionConditionInput, DatasetBasicsFragment, DatasetFlowType } from "src/app/api/kamu.graphql.interface";
-import { logError, promiseWithCatch } from "src/app/common/app.helpers";
+import { DatasetBasicsFragment, DatasetFlowType } from "src/app/api/kamu.graphql.interface";
+import { promiseWithCatch } from "src/app/common/app.helpers";
 import { CompactionTooltipsTexts } from "src/app/common/tooltips/compacting.text";
 import { ModalService } from "src/app/components/modal/modal.service";
-import { CompactionMode, SliceUnit, sliceSizeMapper } from "./dataset-settings-compacting-tab.types";
+import { SliceUnit, sliceSizeMapper } from "./dataset-settings-compacting-tab.types";
 import { DatasetCompactionService } from "../../services/dataset-compaction.service";
 import { DatasetViewTypeEnum } from "src/app/dataset-view/dataset-view.interface";
 import AppValues from "src/app/common/app.values";
@@ -18,20 +18,19 @@ import { BaseComponent } from "src/app/common/base.component";
     styleUrls: ["./dataset-settings-compacting-tab.component.scss"],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DatasetSettingsCompactingTabComponent extends BaseComponent implements OnInit {
+export class DatasetSettingsCompactingTabComponent extends BaseComponent {
     @Input() public datasetBasics: DatasetBasicsFragment;
     public hardCompactionForm = this.fb.group({
-        mode: [CompactionMode.FULL, [Validators.required]],
         sliceUnit: [SliceUnit.MB, [Validators.required]],
         sliceSize: [300, [Validators.required, RxwebValidators.minNumber({ value: 1 })]],
         recordsCount: [10000, [Validators.required, RxwebValidators.minNumber({ value: 1 })]],
-        recursive: [{ value: true, disabled: true }],
+        recursive: [true],
     });
     public readonly SliceUnit: typeof SliceUnit = SliceUnit;
     public readonly MAX_SLICE_SIZE_TOOLTIP = CompactionTooltipsTexts.MAX_SLICE_SIZE;
     public readonly MAX_SLICE_RECORDS_TOOLTIP = CompactionTooltipsTexts.MAX_SLICE_RECORDS;
+    public readonly RECURSIVE_TOOLTIP = CompactionTooltipsTexts.HARD_COMPACTION_RECURSIVE;
     public readonly MIN_VALUE_ERROR_TEXT = "The value must be positive";
-    public readonly CompactionMode: typeof CompactionMode = CompactionMode;
 
     constructor(
         public modalService: ModalService,
@@ -40,14 +39,6 @@ export class DatasetSettingsCompactingTabComponent extends BaseComponent impleme
         private navigationService: NavigationService,
     ) {
         super();
-    }
-
-    ngOnInit(): void {
-        this.hardCompactionModeChanges();
-    }
-
-    public get hardCompactionMode(): AbstractControl {
-        return this.hardCompactionForm.controls.mode;
     }
 
     public get recursive(): AbstractControl {
@@ -81,32 +72,6 @@ export class DatasetSettingsCompactingTabComponent extends BaseComponent impleme
         return this.hardCompactionForm.controls.recordsCount;
     }
 
-    private hardCompactionModeChanges(): void {
-        this.trackSubscription(
-            this.hardCompactionMode.valueChanges.subscribe((value: CompactionMode) => {
-                switch (value) {
-                    case CompactionMode.FULL: {
-                        this.disableAndClearControl(this.recursive);
-                        this.sliceUnit.enable();
-                        this.recordsCount.enable();
-                        this.sliceSize.enable();
-                        break;
-                    }
-                    case CompactionMode.METADATA_ONLY: {
-                        this.recursive.enable();
-                        this.disableAndClearControl(this.sliceUnit);
-                        this.disableAndClearControl(this.recordsCount);
-                        this.disableAndClearControl(this.sliceSize);
-                        break;
-                    }
-                    default: {
-                        logError("Unknown CompactionMode key");
-                    }
-                }
-            }),
-        );
-    }
-
     public onRunCompaction(): void {
         promiseWithCatch(
             this.modalService.error({
@@ -121,9 +86,13 @@ export class DatasetSettingsCompactingTabComponent extends BaseComponent impleme
                                 .runHardCompaction({
                                     datasetId: this.datasetBasics.id,
                                     datasetFlowType: DatasetFlowType.HardCompaction,
-                                    compactionArgs: this.setCompactionArgs(
-                                        this.hardCompactionMode.value as CompactionMode,
-                                    ),
+                                    compactionArgs: {
+                                        full: {
+                                            maxSliceSize: this.sliceSizeInBytes,
+                                            maxSliceRecords: this.hardCompactionForm.controls.recordsCount
+                                                .value as number,
+                                        },
+                                    },
                                 })
                                 .subscribe((result: boolean) => {
                                     if (result) {
@@ -141,29 +110,5 @@ export class DatasetSettingsCompactingTabComponent extends BaseComponent impleme
                 },
             }),
         );
-    }
-
-    private disableAndClearControl(control: AbstractControl): void {
-        control.disable();
-        control.markAsUntouched();
-        control.markAsPristine();
-    }
-
-    private setCompactionArgs(mode: CompactionMode): CompactionConditionInput {
-        switch (mode) {
-            case CompactionMode.FULL:
-                return {
-                    full: {
-                        maxSliceSize: this.sliceSizeInBytes,
-                        maxSliceRecords: this.hardCompactionForm.controls.recordsCount.value as number,
-                    },
-                };
-            case CompactionMode.METADATA_ONLY:
-                return {
-                    metadataOnly: {
-                        recursive: this.recursive.value as boolean,
-                    },
-                };
-        }
     }
 }

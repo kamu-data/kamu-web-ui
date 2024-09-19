56ae5ce646102f8718a63322cf12cb1571bd77ff
Dmitriy Borzenko <borzenko@dnt-lab.com>
2024-09-19 14:30:59 +0300

    Fixed unit tests

--- src/app/dataset-flow/dataset-flow-details/tabs/flow-details-logs-tab/flow-details-logs-tab.component.spec.ts
+++ src/app/dataset-flow/dataset-flow-details/tabs/flow-details-logs-tab/flow-details-logs-tab.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { FlowDetailsLogsTabComponent } from "./flow-details-logs-tab.component";
+import { SharedTestModule } from "src/app/common/shared-test.module";
+import { Apollo } from "apollo-angular";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe("FlowDetailsLogsTabComponent", () => {
     let component: FlowDetailsLogsTabComponent;
@@ -8,6 +11,8 @@ describe("FlowDetailsLogsTabComponent", () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [FlowDetailsLogsTabComponent],
+            imports: [SharedTestModule, HttpClientTestingModule],
+            providers: [Apollo],
         });
         fixture = TestBed.createComponent(FlowDetailsLogsTabComponent);
         component = fixture.componentInstance;

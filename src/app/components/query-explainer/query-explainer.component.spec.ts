f81e10425cb54a0645e429e624ef8aab228ff2ea
Dmitriy Borzenko <borzenko@dnt-lab.com>
2024-10-17 19:55:41 +0300

    fixed unit tests

--- src/app/components/query-explainer/query-explainer.component.spec.ts
+++ src/app/components/query-explainer/query-explainer.component.spec.ts
@@ -1,21 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { QueryExplainerComponent } from "./query-explainer.component";
+import { ActivatedRoute } from "@angular/router";
+import { of } from "rxjs";
+import ProjectLinks from "src/app/project-links";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ToastrModule } from "ngx-toastr";
+import { Apollo } from "apollo-angular";
 
-import { QueryExplainerComponent } from './query-explainer.component';
+describe("QueryExplainerComponent", () => {
+    let component: QueryExplainerComponent;
+    let fixture: ComponentFixture<QueryExplainerComponent>;
 
-describe('QueryExplainerComponent', () => {
-  let component: QueryExplainerComponent;
-  let fixture: ComponentFixture<QueryExplainerComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [QueryExplainerComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [QueryExplainerComponent],
+            imports: [HttpClientTestingModule, ToastrModule.forRoot()],
+            providers: [
+                Apollo,
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        snapshot: {
+                            queryParams: of({
+                                [ProjectLinks.URL_QUERY_PARAM_SQL_QUERY]: `select block_number,to from "kamu/net.rocketpool.reth.tokens-minted" order by offset limit 1`,
+                            }),
+                        },
+                    },
+                },
+            ],
+        });
+        fixture = TestBed.createComponent(QueryExplainerComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(QueryExplainerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/components/query-explainer/query-explainer.service.spec.ts
+++ src/app/components/query-explainer/query-explainer.service.spec.ts
@@ -1,16 +1,19 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import { QueryExplainerService } from "./query-explainer.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ToastrModule } from "ngx-toastr";
 
-import { QueryExplainerService } from './query-explainer.service';
+describe("QueryExplainerService", () => {
+    let service: QueryExplainerService;
 
-describe('QueryExplainerService', () => {
-  let service: QueryExplainerService;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, ToastrModule.forRoot()],
+        });
+        service = TestBed.inject(QueryExplainerService);
+    });
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(QueryExplainerService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it("should be created", () => {
+        expect(service).toBeTruthy();
+    });
 });

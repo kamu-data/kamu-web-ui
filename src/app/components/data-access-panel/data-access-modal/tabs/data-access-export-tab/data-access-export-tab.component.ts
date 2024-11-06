fda5abdd242ebef5122cb06f3ab6889586d8cc8d
Dmitriy Borzenko <borzenko@dnt-lab.com>
2024-11-06 17:53:28 +0200

    minor changes

--- src/app/components/data-access-panel/data-access-modal/tabs/data-access-export-tab/data-access-export-tab.component.ts
+++ src/app/components/data-access-panel/data-access-modal/tabs/data-access-export-tab/data-access-export-tab.component.ts
@@ -1,11 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
-  selector: 'app-data-access-export-tab',
-  templateUrl: './data-access-export-tab.component.html',
-  styleUrls: ['./data-access-export-tab.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: "app-data-access-export-tab",
+    templateUrl: "./data-access-export-tab.component.html",
+    styleUrls: ["./data-access-export-tab.component.scss"],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DataAccessExportTabComponent {
-
-}
+export class DataAccessExportTabComponent {}
--- src/app/components/data-access-panel/data-access-panel.module.ts
+++ src/app/components/data-access-panel/data-access-panel.module.ts
@@ -12,17 +12,28 @@ import { MatDividerModule } from "@angular/material/divider";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { ClipboardModule } from "@angular/cdk/clipboard";
 import { DataAccessModalComponent } from "./data-access-modal/data-access-modal.component";
-import { DataAccessLinkTabComponent } from './data-access-modal/tabs/data-access-link-tab/data-access-link-tab.component';
-import { DataAccessKamuCliTabComponent } from './data-access-modal/tabs/data-access-kamu-cli-tab/data-access-kamu-cli-tab.component';
-import { DataAccessRestTabComponent } from './data-access-modal/tabs/data-access-rest-tab/data-access-rest-tab.component';
-import { DataAccessSqlTabComponent } from './data-access-modal/tabs/data-access-sql-tab/data-access-sql-tab.component';
-import { DataAccessStreamTabComponent } from './data-access-modal/tabs/data-access-stream-tab/data-access-stream-tab.component';
-import { DataAccessCodeTabComponent } from './data-access-modal/tabs/data-access-code-tab/data-access-code-tab.component';
-import { DataAccessOdataTabComponent } from './data-access-modal/tabs/data-access-odata-tab/data-access-odata-tab.component';
-import { DataAccessExportTabComponent } from './data-access-modal/tabs/data-access-export-tab/data-access-export-tab.component';
+import { DataAccessLinkTabComponent } from "./data-access-modal/tabs/data-access-link-tab/data-access-link-tab.component";
+import { DataAccessKamuCliTabComponent } from "./data-access-modal/tabs/data-access-kamu-cli-tab/data-access-kamu-cli-tab.component";
+import { DataAccessRestTabComponent } from "./data-access-modal/tabs/data-access-rest-tab/data-access-rest-tab.component";
+import { DataAccessSqlTabComponent } from "./data-access-modal/tabs/data-access-sql-tab/data-access-sql-tab.component";
+import { DataAccessStreamTabComponent } from "./data-access-modal/tabs/data-access-stream-tab/data-access-stream-tab.component";
+import { DataAccessCodeTabComponent } from "./data-access-modal/tabs/data-access-code-tab/data-access-code-tab.component";
+import { DataAccessOdataTabComponent } from "./data-access-modal/tabs/data-access-odata-tab/data-access-odata-tab.component";
+import { DataAccessExportTabComponent } from "./data-access-modal/tabs/data-access-export-tab/data-access-export-tab.component";
 
 @NgModule({
-    declarations: [DataAccessPanelComponent, DataAccessModalComponent, DataAccessLinkTabComponent, DataAccessKamuCliTabComponent, DataAccessRestTabComponent, DataAccessSqlTabComponent, DataAccessStreamTabComponent, DataAccessCodeTabComponent, DataAccessOdataTabComponent, DataAccessExportTabComponent],
+    declarations: [
+        DataAccessPanelComponent,
+        DataAccessModalComponent,
+        DataAccessLinkTabComponent,
+        DataAccessKamuCliTabComponent,
+        DataAccessRestTabComponent,
+        DataAccessSqlTabComponent,
+        DataAccessStreamTabComponent,
+        DataAccessCodeTabComponent,
+        DataAccessOdataTabComponent,
+        DataAccessExportTabComponent,
+    ],
     imports: [
         CommonModule,
         MatMenuModule,

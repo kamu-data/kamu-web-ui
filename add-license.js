76627d29e5e87ea211cc4e79db066dfb0edc1ca2
Dmitriy Borzenko <borzenko@dnt-lab.com>
2025-02-26 20:56:17 +0200

    set rule for linter

--- add-license.js
+++ add-license.js
@@ -2,13 +2,24 @@ const fs = require("fs");
 const path = require("path");
 const { LICENSE_HEADER } = require("./src/docs/license-header");
 
-const licenseText = LICENSE_HEADER + "\n\n";
-
 function addLicenseToFile(filePath) {
   const content = fs.readFileSync(filePath, "utf8");
+  const LICENSE_HEADER_COUNT_LINES = LICENSE_HEADER.split("\n").length;
   if (!content.startsWith("/**")) {
-    fs.writeFileSync(filePath, licenseText + content, "utf8");
+    fs.writeFileSync(filePath, LICENSE_HEADER + "\n\n" + content, "utf8");
     console.log(`Added license to ${filePath}`);
+  } else {
+    try {
+      const data = fs.readFileSync(filePath, "utf8");
+      const lines = data.split("\n");
+      const newLines = LICENSE_HEADER.split("\n");
+
+      const updatedContent = [...newLines, ...lines.slice(LICENSE_HEADER_COUNT_LINES)].join("\n");
+      fs.writeFileSync(filePath, updatedContent, "utf8");
+      console.log(`First ${LICENSE_HEADER_COUNT_LINES} lines replace in ${filePath}`);
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
 }
 

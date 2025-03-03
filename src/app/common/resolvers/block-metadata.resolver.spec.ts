978edf2f106769cb9b26fdf57cd63bb513b855e5
Dmitriy Borzenko <borzenko@dnt-lab.com>
2025-03-03 18:34:09 +0200

    added tests for search and block-metadata resolvers

--- src/app/common/resolvers/block-metadata.resolver.spec.ts
+++ src/app/common/resolvers/block-metadata.resolver.spec.ts
@@ -6,18 +6,56 @@
  */
 
 import { TestBed } from "@angular/core/testing";
-import { ResolveFn } from "@angular/router";
+import { ActivatedRoute, ActivatedRouteSnapshot, ResolveFn, Router } from "@angular/router";
 import { blockMetadataResolver } from "./block-metadata.resolver";
+import { BlockService } from "src/app/dataset-block/metadata-block/block.service";
+import { Apollo } from "apollo-angular";
+import ProjectLinks from "src/app/project-links";
+import { TEST_ACCOUNT_NAME, TEST_DATASET_NAME } from "src/app/api/mock/dataset.mock";
+import { of } from "rxjs";
 
 describe("blockMetadataResolver", () => {
+    let routeSnapshot: ActivatedRouteSnapshot;
+    let blockService: BlockService;
+    let router: Router;
+    const MOCK_HASH_BLOCK = "weqwKwqeqwdfsdf";
+
     const executeResolver: ResolveFn<void> = (...resolverParameters) =>
         TestBed.runInInjectionContext(() => blockMetadataResolver(...resolverParameters));
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [
+                Apollo,
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        paramMap: {},
+                    },
+                },
+            ],
+        });
+        blockService = TestBed.inject(BlockService);
+        router = TestBed.inject(Router);
     });
 
     it("should be created", () => {
         expect(executeResolver).toBeTruthy();
+    });
+
+    it("should check resolver", async () => {
+        routeSnapshot = new ActivatedRouteSnapshot();
+        routeSnapshot.params = {
+            [ProjectLinks.URL_PARAM_ACCOUNT_NAME]: TEST_ACCOUNT_NAME,
+            [ProjectLinks.URL_PARAM_DATASET_NAME]: TEST_DATASET_NAME,
+            [ProjectLinks.URL_PARAM_BLOCK_HASH]: MOCK_HASH_BLOCK,
+        };
+
+        const requestMetadataBlockSpy = spyOn(blockService, "requestMetadataBlock").and.returnValue(of());
+        await executeResolver(routeSnapshot, router.routerState.snapshot);
+        expect(requestMetadataBlockSpy).toHaveBeenCalledOnceWith(
+            { accountName: TEST_ACCOUNT_NAME, datasetName: TEST_DATASET_NAME },
+            MOCK_HASH_BLOCK,
+        );
     });
 });
--- src/app/common/resolvers/search.resolver.spec.ts
+++ src/app/common/resolvers/search.resolver.spec.ts
@@ -6,18 +6,59 @@
  */
 
 import { TestBed } from "@angular/core/testing";
-import { ResolveFn } from "@angular/router";
+import { ActivatedRoute, ActivatedRouteSnapshot, ResolveFn, Router } from "@angular/router";
 import { searchResolver } from "./search.resolver";
+import ProjectLinks from "src/app/project-links";
+import { SearchService } from "src/app/search/search.service";
+import { Apollo } from "apollo-angular";
 
 describe("searchResolver", () => {
+    let routeSnapshot: ActivatedRouteSnapshot;
+    let router: Router;
+    let searchService: SearchService;
+    const MOCK_PAGE = 2;
+    const MOCK_QUERY = "mock-query";
+
     const executeResolver: ResolveFn<void> = (...resolverParameters) =>
         TestBed.runInInjectionContext(() => searchResolver(...resolverParameters));
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [
+                Apollo,
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        queryParamMap: {},
+                    },
+                },
+            ],
+        });
+        searchService = TestBed.inject(SearchService);
+        router = TestBed.inject(Router);
     });
 
     it("should be created", () => {
         expect(executeResolver).toBeTruthy();
+    });
+
+    it("should check default state for resolver", async () => {
+        routeSnapshot = new ActivatedRouteSnapshot();
+        routeSnapshot.queryParams = {};
+        const searchDatasetsSpy = spyOn(searchService, "searchDatasets");
+        await executeResolver(routeSnapshot, router.routerState.snapshot);
+        expect(searchDatasetsSpy).toHaveBeenCalledOnceWith("", 0);
+    });
+
+    it("should check query parameters state for resolver", async () => {
+        routeSnapshot = new ActivatedRouteSnapshot();
+        routeSnapshot.queryParams = {
+            [ProjectLinks.URL_QUERY_PARAM_PAGE]: MOCK_PAGE.toString(),
+            [ProjectLinks.URL_QUERY_PARAM_QUERY]: MOCK_QUERY,
+        };
+
+        const searchDatasetsSpy = spyOn(searchService, "searchDatasets");
+        await executeResolver(routeSnapshot, router.routerState.snapshot);
+        expect(searchDatasetsSpy).toHaveBeenCalledOnceWith(MOCK_QUERY, MOCK_PAGE - 1);
     });
 });

f025861d0a429bf5e73ebaa32537c69785092eff
Dmitriy Borzenko <borzenko@dnt-lab.com>
2025-03-06 10:42:03 +0200

    added condition for empty result

--- src/app/query-explainer/components/reproduced-result-section/reproduced-result-section.component.spec.ts
+++ src/app/query-explainer/components/reproduced-result-section/reproduced-result-section.component.spec.ts
@@ -8,7 +8,8 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { ReproducedResultSectionComponent } from "./reproduced-result-section.component";
 import { DynamicTableModule } from "src/app/common/components/dynamic-table/dynamic-table.module";
-import { mockQueryExplainerOutput } from "../../query-explainer.mocks";
+import { mockQueryExplainerEmptyOutput, mockQueryExplainerOutput } from "../../query-explainer.mocks";
+import { findElementByDataTestId } from "src/app/common/helpers/base-test.helpers.spec";
 
 describe("ReproducedResultSectionComponent", () => {
     let component: ReproducedResultSectionComponent;
@@ -22,10 +23,17 @@ describe("ReproducedResultSectionComponent", () => {
         fixture = TestBed.createComponent(ReproducedResultSectionComponent);
         component = fixture.componentInstance;
         component.dataJsonAoS = mockQueryExplainerOutput;
-        fixture.detectChanges();
     });
 
     it("should create", () => {
+        fixture.detectChanges();
         expect(component).toBeTruthy();
+    });
+
+    it("should check empty data result", () => {
+        component.dataJsonAoS = mockQueryExplainerEmptyOutput;
+        fixture.detectChanges();
+        const emptyElement = findElementByDataTestId(fixture, "emptyResult");
+        expect(emptyElement?.textContent?.trim()).toEqual("0 records");
     });
 });

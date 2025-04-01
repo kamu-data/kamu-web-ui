ddd14ffc9b5f69edfa77cf1afab1ecc493885617
Dmitriy Borzenko <borzenko@dnt-lab.com>
2025-04-01 14:14:40 +0300

    fixed prettier warning

--- src/app/account/guards/account.guard.spec.ts
+++ src/app/account/guards/account.guard.spec.ts
@@ -1,17 +1,23 @@
-import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+/**
+ * Copyright Kamu Data, Inc. and contributors. All rights reserved.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the LICENSE file.
+ */
 
-import { accountGuard } from './account.guard';
+import { TestBed } from "@angular/core/testing";
+import { CanActivateFn } from "@angular/router";
+import { accountGuard } from "./account.guard";
 
-describe('accountGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => accountGuard(...guardParameters));
+describe("accountGuard", () => {
+    const executeGuard: CanActivateFn = (...guardParameters) =>
+        TestBed.runInInjectionContext(() => accountGuard(...guardParameters));
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+    });
 
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
-  });
+    it("should be created", () => {
+        expect(executeGuard).toBeTruthy();
+    });
 });

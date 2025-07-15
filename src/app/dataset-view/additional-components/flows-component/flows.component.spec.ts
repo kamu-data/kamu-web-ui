9a87c4ca457caae88d9cb4eff32ee6149c58034a
Dmitriy Borzenko <borzenko@dnt-lab.com>
2025-07-15 15:46:37 +0300

    added unit tests

--- src/app/dataset-view/additional-components/flows-component/flows.component.spec.ts
+++ src/app/dataset-view/additional-components/flows-component/flows.component.spec.ts
@@ -26,7 +26,7 @@ import { mockFlowsTableData } from "src/app/api/mock/dataset-flow.mock";
 import { SettingsTabsEnum } from "../dataset-settings-component/dataset-settings.model";
 import { mockDatasetBasicsDerivedFragment } from "src/app/search/mock.data";
 
-fdescribe("FlowsComponent", () => {
+describe("FlowsComponent", () => {
     let component: FlowsComponent;
     let fixture: ComponentFixture<FlowsComponent>;
     let datasetFlowsService: DatasetFlowsService;
@@ -197,6 +197,33 @@ fdescribe("FlowsComponent", () => {
     }));
 
     it("should check 'update now' link for root dataset with PollingSource", fakeAsync(() => {
+        fixture.detectChanges();
+        mockFlowsTableData.connectionDataForWidget.nodes = [];
+        mockFlowsTableData.connectionDataForTable.nodes = [];
+        spyOn(datasetFlowsService, "allFlowsPaused").and.returnValue(of(false));
+        spyOn(datasetFlowsService, "datasetFlowsList").and.returnValue(of(mockFlowsTableData).pipe(delay(10)));
+        spyOn(datasetFlowsService, "flowsInitiators").and.returnValue(of([]));
+        tick(10);
+        fixture.detectChanges();
+        const updateNowClickSpy = spyOn(component, "updateNow");
+        const updateNowLink = findElementByDataTestId(fixture, "update-now-link");
+        expect(updateNowLink).toBeDefined();
+        updateNowLink?.click();
+        expect(updateNowClickSpy).toHaveBeenCalledTimes(1);
+        discardPeriodicTasks();
+    }));
+
+    it("should check 'update now' link for derivative dataset with SetTransform", fakeAsync(() => {
+        const copyMockOverviewUpdate = structuredClone(mockOverviewUpdate);
+        copyMockOverviewUpdate.overview.metadata.currentPollingSource = null;
+        copyMockOverviewUpdate.overview.metadata.currentTransform = {
+            __typename: "SetTransform",
+        };
+        component.flowsData = {
+            datasetBasics: mockDatasetBasicsDerivedFragment,
+            datasetPermissions: mockFullPowerDatasetPermissionsFragment,
+            overviewUpdate: copyMockOverviewUpdate,
+        };
         fixture.detectChanges();
         mockFlowsTableData.connectionDataForWidget.nodes = [];
         mockFlowsTableData.connectionDataForTable.nodes = [];

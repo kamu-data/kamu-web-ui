584b7e1d12c7766faa0643af0b3928177edcc188
Dmitriy Borzenko <borzenko@dnt-lab.com>
2025-07-17 15:14:06 +0300

    added unit tests

--- src/app/common/guards/access-token-expired.guard.spec.ts
+++ src/app/common/guards/access-token-expired.guard.spec.ts
@@ -6,19 +6,28 @@
  */
 
 import { TestBed } from "@angular/core/testing";
-import { CanActivateFn } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot } from "@angular/router";
 import { accessTokenExpiredGuardFn } from "./access-token-expired.guard";
 import { LocalStorageService } from "src/app/services/local-storage.service";
+import { Apollo } from "apollo-angular";
+import { NavigationService } from "src/app/services/navigation.service";
 
 describe("accessTokenExpiredGuard", () => {
     let localStorageService: LocalStorageService;
+    let navigationService: NavigationService;
+
+    const mockRouterStateSnapshot = {} as RouterStateSnapshot;
+    const mockActivatedRouteSnapshot = {} as ActivatedRouteSnapshot;
 
     const executeGuard: CanActivateFn = (...guardParameters) =>
         TestBed.runInInjectionContext(() => accessTokenExpiredGuardFn(...guardParameters));
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [Apollo],
+        });
         localStorageService = TestBed.inject(LocalStorageService);
+        navigationService = TestBed.inject(NavigationService);
     });
 
     it("should be created", () => {
@@ -27,5 +36,18 @@ describe("accessTokenExpiredGuard", () => {
 
     it("should check guard return true", () => {
         localStorageService.setAccessToken(null);
+        const result = executeGuard(mockActivatedRouteSnapshot, mockRouterStateSnapshot);
+        expect(result).toBe(true);
+    });
+
+    it("should check guard return false", () => {
+        const expiredToken =
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkRtaXRyaXkiLCJpYXQiOjE2MDAwMDAwMDAsImV4cCI6MTYwMDAwMDAwMX0.BvM9I0B-_j0XkXnWw9g8kC09fgzFg7UD2CQlj4L5LPk";
+        localStorageService.setAccessToken(expiredToken);
+        const navigateSpy: jasmine.Spy = spyOn(navigationService, "navigateToLogin").and.stub();
+
+        const result = executeGuard(mockActivatedRouteSnapshot, mockRouterStateSnapshot);
+        expect(result).toBe(false);
+        expect(navigateSpy).toHaveBeenCalledTimes(1);
     });
 });

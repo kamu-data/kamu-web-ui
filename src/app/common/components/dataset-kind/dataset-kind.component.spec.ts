f499219f72bc679ec432aaaad79865cce8d8ff61
Dmitriy Borzenko <borzenko@dnt-lab.com>
2025-09-01 11:06:33 +0300

    fixed warning

--- src/app/common/components/dataset-kind/dataset-kind.component.spec.ts
+++ src/app/common/components/dataset-kind/dataset-kind.component.spec.ts
@@ -1,21 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/**
+ * Copyright Kamu Data, Inc. and contributors. All rights reserved.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the LICENSE file.
+ */
 
-import { DatasetKindComponent } from './dataset-kind.component';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-describe('DatasetKindComponent', () => {
-  let component: DatasetKindComponent;
-  let fixture: ComponentFixture<DatasetKindComponent>;
+import { DatasetKindComponent } from "./dataset-kind.component";
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [DatasetKindComponent]
+describe("DatasetKindComponent", () => {
+    let component: DatasetKindComponent;
+    let fixture: ComponentFixture<DatasetKindComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [DatasetKindComponent],
+        });
+        fixture = TestBed.createComponent(DatasetKindComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(DatasetKindComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });

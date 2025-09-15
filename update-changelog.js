b2ba02c0ae32fb92eb80b3214472704cf58ebe98
Dmitriy Borzenko <borzenko@dnt-lab.com>
2025-09-15 13:03:23 +0300

    changed CHANGELOG.md

---
+++ update-changelog.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const { execSync } = require("child_process");
+
+const pkg = JSON.parse(fs.readFileSync("./package.json", "utf8"));
+const version = pkg.version;
+
+const today = new Date().toISOString().split("T")[0]; // YYYY-MM-DD
+const changelogPath = "./CHANGELOG.md";
+
+if (!fs.existsSync(changelogPath)) {
+  console.error("CHANGELOG.md not found");
+  process.exit(1);
+}
+
+const lines = fs.readFileSync(changelogPath, "utf8").split(/\r?\n/);
+
+const lineIndex = 6;
+lines[lineIndex] = `## [${version}] - ${today}`;
+
+fs.writeFileSync(changelogPath, lines.join("\n"));
+
+// change LICENSE.txt
+const licensePath = "./LICENSE.txt";
+
+if (!fs.existsSync(licensePath)) {
+  console.error("LICENSE.txt not found");
+  process.exit(1);
+}
+
+const linesLicense = fs.readFileSync(licensePath, "utf8").split(/\r?\n/);
+
+const lineIndexLicense = 13;
+linesLicense[lineIndexLicense] = `Licensed Work:             Kamu Web UI Version ${version}`;
+fs.writeFileSync(licensePath, linesLicense.join("\n"));
+
+// Commit changes
+execSync("git add LICENSE.txt CHANGELOG.md", { stdio: "inherit" });
+execSync(`git commit -m "Release v${version}"`, { stdio: "inherit" });

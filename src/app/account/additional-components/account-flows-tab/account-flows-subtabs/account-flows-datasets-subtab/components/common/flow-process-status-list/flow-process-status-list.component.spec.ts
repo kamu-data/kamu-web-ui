d5ae81a1f8ce8ade8be88a56d7a532270eac6912
dmitriy-borzenko <dmytro.borzenko@kamu.dev>
2025-12-08 20:12:56 +0200

    minor changes

--- src/app/account/additional-components/account-flows-tab/account-flows-subtabs/account-flows-datasets-subtab/components/common/flow-process-status-list/flow-process-status-list.component.spec.ts
+++ src/app/account/additional-components/account-flows-tab/account-flows-subtabs/account-flows-datasets-subtab/components/common/flow-process-status-list/flow-process-status-list.component.spec.ts
@@ -1,21 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/**
+ * Copyright Kamu Data, Inc. and contributors. All rights reserved.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the LICENSE file.
+ */
 
-import { FlowProcessStatusListComponent } from './flow-process-status-list.component';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { FlowProcessStatusListComponent } from "./flow-process-status-list.component";
 
-describe('FlowProcessStatusListComponent', () => {
-  let component: FlowProcessStatusListComponent;
-  let fixture: ComponentFixture<FlowProcessStatusListComponent>;
+describe("FlowProcessStatusListComponent", () => {
+    let component: FlowProcessStatusListComponent;
+    let fixture: ComponentFixture<FlowProcessStatusListComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [FlowProcessStatusListComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [FlowProcessStatusListComponent],
+        });
+        fixture = TestBed.createComponent(FlowProcessStatusListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(FlowProcessStatusListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });

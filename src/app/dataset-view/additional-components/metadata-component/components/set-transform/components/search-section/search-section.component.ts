d3e55c7e85e1d3b55d761f704d63da50d9286547
dmitriy-borzenko <dmytro.borzenko@kamu.dev>
2026-01-09 15:10:20 +0200

    the view for the SetTransform event has been corrected

--- src/app/dataset-view/additional-components/metadata-component/components/set-transform/components/search-section/search-section.component.ts
+++ src/app/dataset-view/additional-components/metadata-component/components/set-transform/components/search-section/search-section.component.ts
@@ -16,7 +16,7 @@ import { SearchApi } from "src/app/api/search.api";
 import { MaybeNull } from "src/app/interface/app.types";
 import AppValues from "src/app/common/values/app.values";
 import { DatasetService } from "src/app/dataset-view/dataset.service";
-import { DatasetSchema } from "src/app/interface/dataset.interface";
+import { DataSchemaTypeField, DatasetSchema } from "src/app/interface/dataset.interface";
 import { DatasetAutocompleteItem, TypeNames } from "src/app/interface/search.interface";
 import { DatasetNode } from "../../set-transform.types";
 import { BaseComponent } from "src/app/common/components/base.component";
@@ -29,6 +29,7 @@ import { MatButtonModule } from "@angular/material/button";
 import { NgIf } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { MatIconModule } from "@angular/material/icon";
+import { OdfTypeMapper } from "src/app/common/helpers/data.helpers";
 
 @Component({
     selector: "app-search-section",
@@ -149,5 +150,9 @@ export class SearchSectionComponent extends BaseComponent {
 
     public hasChild(_: number, node: DatasetNode): boolean {
         return !!node.children && node.children.length > 0;
+    }
+
+    public OdfTypeMapper(type: DataSchemaTypeField): string {
+        return OdfTypeMapper(type);
     }
 }
